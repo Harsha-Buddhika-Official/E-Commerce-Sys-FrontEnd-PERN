843fe55d7d849b79de3487a5d0dc4e5aa7e3fdf9
Harsha Buddhika <59781888+Harsha-Buddhika-Official@users.noreply.github.com>
2026-06-06 12:19:56 +0530

    Update banner.api.js

--- src/modules/admin/features/banners/api/banner.api.js
+++ src/modules/admin/features/banners/api/banner.api.js
@@ -9,7 +9,7 @@ export const getAllBannersApi = async () =>{
 export const getBannerByIdApi = async (id) =>{
   console.log("Fetching banner with ID:", id);
   return API.get(`/banners/${id}`);
-}
+} 
 
 // Create banner
 export const createBannerApi = async (formData) => {
