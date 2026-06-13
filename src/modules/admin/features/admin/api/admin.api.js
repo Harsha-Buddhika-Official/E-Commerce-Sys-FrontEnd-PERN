aa887c6e50595c2386c72ded1b9b3c4c79a8e2c2
Harsha Buddhika <59781888+Harsha-Buddhika-Official@users.noreply.github.com>
2026-06-13 16:18:29 +0530

    Update admin.api.js

--- src/modules/admin/features/admin/api/admin.api.js
+++ src/modules/admin/features/admin/api/admin.api.js
@@ -1,26 +1,31 @@
 import API from "../../../../../api/client";
 
-// to create a new admin in admin management page //successful
-export const createAdmin = (payload) => {
-    return API.post("/admin/register", payload).then((res) => res.data);
+// Create a new admin (Admin Management page)
+export const createAdmin = async (payload) => {
+    const res = await API.post("/admin/register", payload);
+    return res.data;
 };
 
-// to get all admins in admin management page //successful
-export const getAllAdmins = () => {
-    return API.get("/admin").then((res) => res.data);
+// to get all admins (admin management page) 
+export const getAllAdmins = async () => {
+    const res = await API.get("/admin");
+    return res.data;
 };
 
-// to update admin role in admin management page //successful
-export const updateAdminRole = (adminId, newRole) => {
-    return API.put(`/admin/updateRole/${adminId}`, { newRole }).then((res) => res.data);
+// to update admin role (admin management page) 
+export const updateAdminRole = async (adminId, newRole) => {
+    const res = await API.put(`/admin/updateRole/${adminId}`, { newRole });
+    return res.data;
 };
 
-// to update admin password in admin settings page //successful
-export const updatePassword = (adminId, passwordData) => {
-    return API.put(`/admin/settings/updatePassword/${adminId}`, { passwordData }).then((res) => res.data);
+// to update admin password (admin settings page) 
+export const updatePassword = async (adminId, passwordData) => {
+    const res = await API.put(`/admin/settings/updatePassword/${adminId}`, { passwordData });
+    return res.data;
 };
 
-// to delete an admin by email in admin management page //successful
-export const deleteAdminByEmail = (email) => {
-    return API.delete("/admin/delete", { data: { email } }).then((res) => res.data);
+// to delete an admin by email (admin management page) 
+export const deleteAdminByEmail = async (email) => {
+    const res = await API.delete("/admin/delete", { data: { email } });
+    return res.data;
 };
