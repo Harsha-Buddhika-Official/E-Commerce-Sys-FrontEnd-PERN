18547c5a38c0215c3c9dbebc39fc5ff8fafbf171
Harsha Buddhika <59781888+Harsha-Buddhika-Official@users.noreply.github.com>
2026-07-05 09:51:11 +0530

    Update order.api.js

--- src/modules/public/features/orders/api/order.api.js
+++ src/modules/public/features/orders/api/order.api.js
@@ -1,7 +1,6 @@
 import API from "../../../../../api/client";
 
 export const trackOrderAPI = async (payload) => {
-    // console.log("API call - Tracking order with:", payload); // Debug log
     const response = await API.post("/orders/tracking", payload);
     return response.data;
 };
@@ -9,14 +8,12 @@ export const trackOrderAPI = async (payload) => {
 
 // Create Order (Direct or Cart)
 export const createOrderApi = (payload) =>{
-    // console.log("API call - Creating order with payload:", payload); // Debug log
     return API.post("/orders/create", payload,{
         timeout: 100000, // Set timeout to 100 seconds
     });
 }
 
 export const uploadReceiptAPI = async (orderId, formData) => {
-    // console.log("API call - Uploading receipt for orderId:", orderId, "with formData:", formData); // Debug log
     const { data } = await API.post(
         `/orders/upload-receipt/${orderId}`,
         formData,
