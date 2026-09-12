9d8947fe71fba08cc0656f56e5965788405f61de
Harsha Buddhika <59781888+Harsha-Buddhika-Official@users.noreply.github.com>
2026-09-12 09:41:49 +0530

    cached successful in public side

--- src/modules/public/features/banners/hooks/useImageBanner.js
+++ src/modules/public/features/banners/hooks/useImageBanner.js
@@ -1,19 +1,30 @@
 import { useState, useEffect } from 'react';
 import { fetchImageBanners } from '../services/banners.service.js';
+import { getCache, setCache } from "../../../../../utils/cache.js";
 
 export const useImageBanner = () => {
     const [banners, setBanners] = useState([]);
     const [loading, setLoading] = useState(true);
     const [error, setError] = useState(null);
 
+    const cacheKey = "image_banners";
+
     const getAllBanners = async () => {
         try {
             setLoading(true);
             setError(null);
 
+            const cachedData = getCache(cacheKey);
+            if (cachedData) {
+                setBanners(cachedData);
+                setLoading(false);
+                return;
+            }
+
             const data = await fetchImageBanners();
             // console.log('Fetched image banners:', data);
             setBanners(data);
+            setCache(cacheKey, data);
         } catch (error) {
             console.error('Error fetching image banners:', error);
             setError(error.message || 'Failed to load image banners');
